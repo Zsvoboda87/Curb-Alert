ef7ca337e7237f12f6c40b080ca057b9f36576d9
Zsvob <svobodazachary@gmail.com>
2022-04-28 11:23:21 -0400

    change http

--- curb-alert/src/App.js
+++ curb-alert/src/App.js
@@ -11,7 +11,7 @@ import { setContext } from '@apollo/client/link/context';
 export const customHistory = createBrowserHistory();
 
 const httpLink = createHttpLink({
-  uri: 'http://localhost:3001/graphql',
+  uri: '/graphql',
 });
 
 const authLink = setContext((_, { headers }) => {
