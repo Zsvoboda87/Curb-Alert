1212621662c2784a0b659d93d296e1ab80f7a43b
perfect perfect <gilberto.j.covarrubias@gmail.com>
2022-04-30 22:43:10 -0400

    Rought draft of user profile finished

--- curb-alert/src/components/Profile/index.js
+++ curb-alert/src/components/Profile/index.js
@@ -5,15 +5,29 @@ import UpdateProfile from '../UpdateProfile'
 import Login from '../Login';
 import SignUp from '../Signup'
 import Auth from '../../utils/auth';
+import { useQuery } from '@apollo/client';
+import { QUERY_ME_BASIC } from '../../utils/queries';
+import { Link } from 'react-router-dom';
 
 function Profile() {
+
+    const { data: userData } = useQuery(QUERY_ME_BASIC);
+    const loggedIn = Auth.loggedIn();
 
     const { logout } = Auth;
 
     return (
         <div className="profile-header">
             <img className="profileImage" src="../images/avatar.png"></img>
-            <Button id="button">View Profile</Button>
+            {loggedIn && userData ? (
+                <div>
+                    <h5>{userData.me.username}</h5>
+                </div>
+            ) : null}
+            <Link id="link" to={`/userprofile`}>
+                View Profile
+            </Link>
+
             <Button id="button">Messages</Button>
             <AddPost />
             <UpdateProfile />
