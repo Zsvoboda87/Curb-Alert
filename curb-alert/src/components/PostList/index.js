9b48b9b99bab067063c966c5e5e1e32c1aa520db
perfect perfect <gilberto.j.covarrubias@gmail.com>
2022-05-03 14:00:20 -0400

    Trouble shooting delete post

--- curb-alert/src/components/PostList/index.js
+++ curb-alert/src/components/PostList/index.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import { Card } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import { useMutation } from '@apollo/client';
+import { REMOVE_POST } from '../../utils/mutations';
+import { Button } from 'react-bootstrap';
+
 
 const PostList = ({ posts, title }) => {
-
+    const [removePost, { error }] = useMutation(REMOVE_POST);
+    
     // if (!posts.length)
+
+    const handleDelete = async (postId) => {
+        console.log(postId)
+        try {
+            await removePost({
+                variables: { _id: postId }
+            });
+        } catch (e) {
+            console.error(e);
+        }
+    }
 
     return (
         <div>
@@ -15,14 +31,16 @@ const PostList = ({ posts, title }) => {
                 {' '}
                 {posts &&
                     posts.map(post => (
-                        <Link to={`/post/${post._id}`}>
-                            < Card id="feed-item" className="flex-center" style={{ width: '23vw', height: '50vh', margin: '.5rem' }}>
+                        
+                        < Card id="feed-item" className="flex-center" style={{ width: '23vw', height: '50vh', margin: '.5rem' }}>
+                            <Link to={`/post/${post._id}`}>
                                 <Card.Title>{post.itemTitle}</Card.Title>
                                 <Card.Img id="card-img" style={{ width: '98%', height: '45vh', margin: 'auto' }}
-                                    variant="top" src={post.imageURL} />
-
-                            </Card >
-                        </Link>
+                                    variant="top" src={post.imageURL} 
+                                />
+                            </Link>
+                            <Button id="button" onClick={handleDelete(post._id)}>Delete Post</Button>
+                        </Card >
                     ))
                 }
             </div>
